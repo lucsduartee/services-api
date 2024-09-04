60e9aa04184275b3dc63263dc11bed91666500ad
Lucas Duarte <lucs.rkt@gmail.com>
2024-09-04 17:45:10 -0300

    add prod client url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:3000",
-            "http://localhost:3001"
+            "http://localhost:3001",
+            "https://service-management-erp.vercel.app/"
 
 
     resource "*",
